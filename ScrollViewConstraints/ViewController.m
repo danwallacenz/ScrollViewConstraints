36f155381e544369a69dfed79dc6f0ca524eb87b
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-09-09 23:56:31 +1200

    injitial commit

--- ScrollViewConstraints/ViewController.m
+++ ScrollViewConstraints/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIView *contentView;
 @end
 
 @implementation ViewController
@@ -18,6 +19,61 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+
+    CGFloat contentWidth,contentHeight;
+    BOOL layoutIsPortrait = UIDeviceOrientationIsPortrait(self.interfaceOrientation);
+    if(layoutIsPortrait){
+        contentWidth = self.view.bounds.size.width;
+        contentHeight = self.view.bounds.size.height;
+    }else{
+        contentWidth = self.view.bounds.size.height;
+        contentHeight = self.view.bounds.size.width;
+    }
+    
+
+    
+    self.contentView = [[UIView alloc] initWithFrame:CGRectMake(0,0,contentWidth,contentHeight * 2)];
+
+    [self.contentView setBackgroundColor:[UIColor blueColor]];
+    
+    [self.scrollView addSubview:self.contentView];
+    
+    // DON'T change contentView's translatesAutoresizingMaskIntoConstraints,
+    // which defaults to YES;
+    
+    // Set the content size of the scroll view to match the size of the content view:
+    [self.scrollView setContentSize:CGSizeMake(self.contentView.bounds.size.width,self.contentView.bounds.size.height)];
+    
+}
+
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    CGFloat contentWidth,contentHeight;
+    BOOL layoutWillBePortrait = UIDeviceOrientationIsPortrait(toInterfaceOrientation);
+    if(layoutWillBePortrait){
+        contentWidth = self.view.bounds.size.width;
+        contentHeight = self.view.bounds.size.height;
+    }else{
+        contentWidth = self.view.bounds.size.height;
+        contentHeight = self.view.bounds.size.width;
+    }
+    
+    self.contentView.frame = CGRectMake(0,0,contentWidth,contentHeight * 2);
+    [self.scrollView setContentSize:CGSizeMake(self.contentView.bounds.size.width,self.contentView.bounds.size.height)];
+}
+
+
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    CGSize deviceSize = DEVICE_SIZE;
+    
+    NSLog(@"DEVICE_SIZE=%@",NSStringFromCGSize(deviceSize));
 }
 
 - (void)didReceiveMemoryWarning
